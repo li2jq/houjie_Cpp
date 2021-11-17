ee16e346035b0bf83afa6ba019b541cdc5a3358b
li2jq <li2jq@mail.uc.edu>
2021-11-17 13:48:11 +0800

    Basic Test For Complex

--- src/complex_test.cpp
+++ src/complex_test.cpp
@@ -1,1 +1,15 @@
 #include <iostream>
+#include "../include/complex.hpp"
+
+using namespace std;
+
+int main()
+{
+    Complex c1, c2(5, 10);
+    cout << "c2: "<< c2 << endl;
+
+    c1 += c2;
+
+    cout << "c1: " << c1 << endl;
+
+};
