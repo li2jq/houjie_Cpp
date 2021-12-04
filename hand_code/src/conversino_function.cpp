881041972f4bde4758afab7f1f58a8ce07e97cc4
li2jq <li2jq@mail.uc.edu>
2021-12-04 10:50:33 +0800

    Conversion and non-explicit one-argument ctor

--- hand_code/src/conversino_function.cpp
+++ hand_code/src/conversino_function.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <typeinfo>
 using namespace std;
 
 class fraction
@@ -7,19 +7,53 @@ class fraction
 private:
     int Numerator, Denominator;
 public:
-    fraction (int Num, int Deno=1 ):Numerator(Num),Denominator(Deno) {};
+    explicit fraction (int Num, int Deno=1 ):Numerator(Num),Denominator(Deno) {
+        cout << "non-explicit-one-argument-used" << endl;
+    };
 
     operator double() const
     {
-        return Numerator / (double) Denominator;
+        cout << "double conversion used" << endl;
+        return (double)Numerator / Denominator;
     }
-};
 
+    fraction operator+(const int& i)
+    {
+        int Deno = this->Denominator;
+        int Num = this->Numerator* i;
+
+        cout << "Hello Operator +" << endl;
+        return fraction(Num, Deno);
+
+    }
+
+        fraction operator+(const fraction& f)
+    {
+        int Deno = this->Denominator * f.Denominator;
+        int Num = this->Numerator* f.Denominator + this->Denominator*f.Numerator;
+
+        cout << "Hello Operator +" << endl;
+        return fraction(Num, Deno);
+
+    }
+
+    friend ostream& operator << (ostream& os, const fraction& f);
+    
+};
+ostream& operator << (ostream& os, const fraction& f)
+{
+    return os << "The numerator is " << f.Numerator << " and The denominator is " << f.Denominator << endl;
+}
 
 
 int main()
 {
     fraction f(3, 5);
-    double a = 4 + f;
+    cout << "Hello fffff" << endl;
+
+    fraction a = f + 4;
+
+    cout << "Hello definition a" << endl;
     cout << "a is " << a << endl;
+    // cout << typeid(4+f).name() << endl;
 }
